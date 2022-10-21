aeac5662e0d79197a9178efb74e7ffff06ca64a4
razinogriffith <abdallah.harraki@gmail.com>
2022-10-21 16:59:00 +0100

    refactoring interface to base. closes #2

---
+++ src/main/java/sfg/springframwork/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,6 @@
+package sfg.springframwork.spring5jokesappv2.services;
+
+public interface JokeService {
+
+     String GetJoke();
+}
---
+++ src/main/java/sfg/springframwork/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package sfg.springframwork.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JokeServiceImpl implements JokeService {
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes =  new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String GetJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
